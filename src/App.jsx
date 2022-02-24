4f15b2819229eb8a4c196f04d8bd3a3ab34488c9
pedro-git-projects <pedro.generic.email@gmail.com>
2022-02-24 08:36:16 -0300

    exact routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Switch, Route, Link, useParams } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch } from "react-router-dom";
 import Movies from "./components/Movies.jsx";
 import Admin from "./components/Admin.jsx";
 import Home from "./components/Home.jsx";
+import Categories from "./components/Categories.jsx";
+
+/*
+ * We route by wrapping our content in a router and creating <Link to=""> tags
+ * To make this routes work, however, we need to wrap the tags <Route path="">
+ * in a pair of <Switch> tags
+ * Note that order matters in the switch
+ * */
 
 export default function App() {
 	return (
@@ -25,6 +33,9 @@ export default function App() {
 									<Link to="/movies">Movies</Link>
 								</li>
 								<li className="list-group-item">
+									<Link to="/by-category">Categories</Link>
+								</li>
+								<li className="list-group-item">
 									<Link to="/admin">Manage Catalogue</Link>
 								</li>
 							</ul>
@@ -33,16 +44,29 @@ export default function App() {
 					<div className="col-md-10">
 						<Switch>
 							<Route path="/movies/:id">
-								<Movie/>
+								<Movie />
 							</Route>
 							<Route path="/movies">
-								<Movies/>
+								<Movies />
 							</Route>
+							<Route exact path="/by-category">
+								<CategoryPage />
+							</Route>
+							<Route 
+								exact
+								path="/by-category/drama"
+								render={(props) => <Categories {...props} title={`Drama`} />}
+							/>
+							<Route 
+								exact
+								path="/by-category/comedy"
+								render={(props) => <Categories {...props} title={`Comedy`} />}
+							/>
 							<Route path="/admin">
-								<Admin/>
+								<Admin />
 							</Route>
 							<Route path="/">
-								<Home/>
+								<Home />
 							</Route>
 						</Switch>
 					</div>
@@ -56,4 +80,27 @@ export default function App() {
 function Movie() {
 	let { id } = useParams()
 	return <h2>Movie id {id}</h2>
+}
+
+function CategoryPage() {
+	// relative paths, relative links
+	let { path, url } = useRouteMatch()
+	return (
+		<div>
+			<h2>Categories</h2>
+			<ul>
+				<li>
+					<Link to={`${path}/drama`}> Action </Link>
+				</li>
+
+				<li>
+					<Link to={`${path}/drama`}> Comedy </Link>
+				</li>
+
+				<li>
+					<Link to={`${path}/drama`}> Drama </Link>
+				</li>
+			</ul>
+		</div>		
+	)
 }
